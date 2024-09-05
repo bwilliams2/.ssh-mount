bf817c96fb7246183b7e07dcc5dc3fecb6e18fe5
Bryce Williams <bwilliams2@gmail.com>
2024-09-05 13:03:59 -0500

    Update test check

--- test/sshmount/random.sh
+++ test/sshmount/random.sh
@@ -10,7 +10,7 @@ source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check test -f "$HOME/.ssh/smoke-test"
+check "check file exists" test -f "$HOME/.ssh/smoke-test"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
--- test/sshmount/test.sh
+++ test/sshmount/test.sh
@@ -3,14 +3,14 @@
 # This test file will be executed against one of the scenarios devcontainer.json test that
 # includes the 'color' feature with "favorite": "gold" option.
 
-set -e
+set -x
 
 # Optional: Import test library bundled with the devcontainer CLI
 source dev-container-features-test-lib
 
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib.
-check test -f "$HOME/.ssh/smoke-test"
+check "check file exists" test -f "$HOME/.ssh/smoke-test"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
