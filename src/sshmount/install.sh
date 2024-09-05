455755042e82f378f5fa95d41a24a17c0d1f53c4
Bryce Williams <bwilliams2@gmail.com>
2024-09-05 13:35:21 -0500

    Update install

--- src/sshmount/install.sh
+++ src/sshmount/install.sh
@@ -4,4 +4,4 @@ echo "Installing sshmount"
 
 ln -s /mnt/.ssh $_REMOTE_USER_HOME/.ssh
 chown -R $_REMOTE_USER:$_REMOTE_USER $_REMOTE_USER_HOME/.ssh
-chmod -R u+r $_REMOTE_USER_HOME/.ssh
+# chmod -R u+r $_REMOTE_USER_HOME/.ssh
